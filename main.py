54c87ea9a4e4cfad6cf9b4e4f885e5f4c8ff4299
Allen Liu <166740638+mraliu@users.noreply.github.com>
2024-05-18 09:44:20 +0100

    Update main.py
    Uses dictionary to store key pair value

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ def getcolour():
     return f'#{rgb[0]:02x}{rgb[1]:02x}{rgb[2]:02x}'
 
 def switchpage(pagenum):
-    [frame.place_forget() for frame in frames]
-    pageLbl.config(text=pages[pagenum])
+    [frames[frame].place_forget() for frame in frames]
+    pageLbl.config(text=pagenum)
     frames[pagenum].place(x=0,y=50, width=800, height=550)
 
 # Tk Main Window
@@ -16,7 +16,7 @@ root.geometry("800x600")
 
 # Pages stored in array
 pages = ["Homepage", "Products", "Checkout", "Help"]
-frames = [tk.Frame(root, bg=getcolour()) for page in pages]
+frames = {page:tk.Frame(root, bg=getcolour()) for page in pages}
 
 # File menu
 filemenu = tk.Menu(root)
@@ -24,11 +24,10 @@ root.config(menu=filemenu)
 # Sub menu
 subfilemenu = tk.Menu(filemenu)
 filemenu.add_cascade(label="Pages", menu=subfilemenu)
-[subfilemenu.add_command(label=page, command=lambda p=idx:switchpage(p)) for idx, page in enumerate(pages)] # Pages from loop
+[subfilemenu.add_command(label=page, command=lambda p=page:switchpage(p)) for page in pages] # Pages from loop
 subfilemenu.add_command(label="Exit", command=root.quit)
 
 pageLbl = tk.Label(root, text="", font="Arial 28")
 pageLbl.place(x=50,y=0)
 
 root.mainloop()
-
