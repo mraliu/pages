0020bba249e34ca11b02fb9de1e78d8ba11d3b02
Allen Liu <166740638+mraliu@users.noreply.github.com>
2024-05-18 09:25:23 +0100

    Create main.py
    Multi-frame window

---
+++ main.py
@@ -0,0 +1,34 @@
+import tkinter as tk, random
+
+def getcolour():
+    rgb = [random.randint(0,255), random.randint(0,255), random.randint(0,255)]
+    return f'#{rgb[0]:02x}{rgb[1]:02x}{rgb[2]:02x}'
+
+def switchpage(pagenum):
+    [frame.place_forget() for frame in frames]
+    pageLbl.config(text=pages[pagenum])
+    frames[pagenum].place(x=0,y=50, width=800, height=550)
+
+# Tk Main Window
+root = tk.Tk()
+root.title("DB window")
+root.geometry("800x600")
+
+# Pages stored in array
+pages = ["Homepage", "Products", "Checkout", "Help"]
+frames = [tk.Frame(root, bg=getcolour()) for page in pages]
+
+# File menu
+filemenu = tk.Menu(root)
+root.config(menu=filemenu)
+# Sub menu
+subfilemenu = tk.Menu(filemenu)
+filemenu.add_cascade(label="Pages", menu=subfilemenu)
+[subfilemenu.add_command(label=page, command=lambda p=idx:switchpage(p)) for idx, page in enumerate(pages)] # Pages from loop
+subfilemenu.add_command(label="Exit", command=root.quit)
+
+pageLbl = tk.Label(root, text="", font="Arial 28")
+pageLbl.place(x=50,y=0)
+
+root.mainloop()
+
